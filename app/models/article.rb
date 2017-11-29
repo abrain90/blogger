20f75205731cba0869f87eb90fff232c0c146547
Andrew Brain <abrain@mymacleay.edu.au>
2017-11-29 04:18:07 +0000

    finish blog comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,12 @@
-class Article < ApplicationRecord
-end
-a = Article.new
-a.title = "Sample Article Title"
-a.body = "This is the text for my article, woo hoo!"
-a.save
-Article.all
+#class Article < ApplicationRecord
+#end
 
+#a = Article.new
+#a.title = "Sample Article Title"
+#a.body = "This is the text for my article, woo hoo!"
+#a.save
+#Article.all
+
+class Article < ActiveRecord::Base
+    has_many :comments
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171128023916) do
+ActiveRecord::Schema.define(version: 20171128223238) do
 
   create_table "artcles", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 20171128023916) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "author_name"
+    t.text "body"
+    t.integer "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
 end
